e99fb8474623738d4513d26113bb42b33c894c02
Dkhoatp <duckhoatrpt@gmail.com>
2021-09-22 19:47:30 +0700

    Up file chua src code java + sql lan dau tien

---
+++ HuongRungBuffet/src/huongrungbuffet/HuongRungBuffet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package huongrungbuffet;
+
+/**
+ *
+ * @author Dell
+ */
+public class HuongRungBuffet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
